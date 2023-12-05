2bcb2cada3a46994391e7331d23e20a16b67c183
Matthew Toal <112990339+Mtoal087@users.noreply.github.com>
2023-12-05 17:22:39 -0600

    Delete second.py, not in use

--- second.py
+++
@@ -1,6 +0,0 @@
-# for options function
-# allows the user to navigate through Iotta
-# need a settings func to possibly rename, password, or delete account
-# create creature
-# leaderboard
-# etc...
